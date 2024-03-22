b6368a843619277956df2157ffc04a7e0c41c1ea
Syed Uns Haider Zaidi <unshaider28@gmail.com>
2024-03-23 03:28:11 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "guessNumber",
+        message: "Guess a number between 1 to 10",
+    },
+]);
+if (answers.guessNumber === randomNumber) {
+    console.log("Congratulations! You guess right number.");
+}
+else {
+    console.log(`You guess wrong number\nThe right number is ${randomNumber}.`);
+}
---
+++ main.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+  {
+    type: "number",
+    name: "guessNumber",
+    message: "Guess a number between 1 to 10",
+  },
+]);
+
+if (answers.guessNumber === randomNumber) {
+    console.log("Congratulations! You guess right number.");
+} else {
+    console.log(`You guess wrong number\nThe right number is ${randomNumber}.`);
+}
