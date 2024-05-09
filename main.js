acd8e08f2a08c09674f20cecaadb9cd4cfc10900
Syed Uns Haider Zaidi <unshaider28@gmail.com>
2024-05-10 03:12:00 +0500

    first commit

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 const randomNumber = Math.floor(Math.random() * 10 + 1);
-const answers = await inquirer.prompt([
-    {
-        type: "number",
-        name: "guessNumber",
-        message: "Guess a number between 1 to 10",
-    },
-]);
-if (answers.guessNumber === randomNumber) {
-    console.log("Congratulations! You guess right number.");
-}
-else if (answers.guessNumber > randomNumber) {
-    console.log(`Your guess is high.\nRight guess is ${randomNumber}.`);
-}
-else if (answers.guessNumber < randomNumber) {
-    console.log(`Your guess is low.\nRight guess is ${randomNumber}.`);
+console.log(chalk.blue("Welcome to the Number Guessing Game!"));
+console.log(chalk.gray("I've picked a number between 1 and 10. Try to guess it!"));
+let attempts = 0;
+while (true) {
+    attempts++;
+    const answers = await inquirer.prompt([
+        {
+            type: "number",
+            name: "guessNumber",
+            message: "Enter your guess:",
+        },
+    ]);
+    if (answers.guessNumber === randomNumber) {
+        console.log(chalk.green(`Congratulations! You've guessed the number in ${attempts} attempts.`));
+        break;
+    }
+    else if (answers.guessNumber > randomNumber) {
+        console.log(chalk.red("Too high! Try again."));
+    }
+    else if (answers.guessNumber < randomNumber) {
+        console.log(chalk.red("Too low! Try again."));
+    }
 }
